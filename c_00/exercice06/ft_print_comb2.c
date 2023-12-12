346505372a4cd55d9145b983e3bd5a4a9d15e95d
Frédéric FOISSEY <AJ1032485@tdf.hubtotal.net>
2023-12-12 14:58:17 +0100

    ft_print_comb2.c

--- c_00/exercice06/ft_print_comb2.c
+++ c_00/exercice06/ft_print_comb2.c
@@ -1,44 +1,71 @@
 #include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-void ft_putchar(char c)
+#define SEPARATOR ','
+#define LAST_SECOND_NB 99
+#define LAST_FIRST_NB (LAST_SECOND_NB - 1)
+#define ASCII_DIGIT_OFFSET 48
+
+static ssize_t ft_putchar(const char c)
 {
-	write(1, &c, 1);
+	return write(STDOUT_FILENO, &c, sizeof(c));
 }
+
+static void ft_print_space(void)
+{
+	ft_putchar(' ');
+}
+
+static void ft_print_endl(void)
+{
+	ft_putchar('\n');
+}
+
+static bool is_last_numbers(const uint8_t a, const uint8_t b)
+{
+	return (a == LAST_FIRST_NB && b == LAST_SECOND_NB);
+}
+
+static void ft_print_numbers_separator(void)
+{
+	ft_putchar(SEPARATOR);
+	ft_print_space();
+}
+
+static void ft_print_nb(const uint8_t nb)
+{
+	ft_putchar(nb / 10 + ASCII_DIGIT_OFFSET);
+	ft_putchar(nb % 10 + ASCII_DIGIT_OFFSET);
+}
+
 
 void ft_print_comb2(void)
 {
-	int a;
-	int b;
+	uint8_t a;
+	uint8_t b;
 
 	a = 0;
-	b = 1;
-
-	while (a <= 98)
+	while (a <= LAST_FIRST_NB)
 	{
-		while (b <= 99)
-		{
-			ft_putchar(a / 10 + '0');
-			ft_putchar(a % 10 + '0');
-			ft_putchar(' ');
-			ft_putchar(b / 10 + '0');
-			ft_putchar(b % 10 + '0');
-			
-			if (!(a == 98 && b == 99))
-			{
-				write(1, ", ", 2);
-			}
-			b++;
-		}
-		a++;
 		b = a + 1;
+		while (b <= LAST_SECOND_NB)
+		{
+			ft_print_nb(a);
+			ft_print_space();
+			ft_print_nb(b);
+			if (is_last_numbers(a, b) == false)
+				ft_print_numbers_separator();
+			++b;
+		}
+		++a;
 	}
 }		
 
 int main(void)
 {
 	ft_print_comb2();
-
-	ft_putchar('\n');
-
-	return 0;
+	ft_print_endl();
+	return EXIT_SUCCESS;
 }
