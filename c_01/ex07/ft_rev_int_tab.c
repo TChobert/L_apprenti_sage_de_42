935687a91bf772c3f232f1718696edd2a7c1df79
Frédéric FOISSEY <AJ1032485@tdf.hubtotal.net>
2023-12-14 15:22:27 +0100

    correc c01 ex07

--- c_01/ex07/ft_rev_int_tab.c
+++ c_01/ex07/ft_rev_int_tab.c
@@ -3,7 +3,7 @@
 
 void ft_swap(int *a, int *b)
 {
-	int valeur_de_a = *a;
+	const int valeur_de_a = *a;
 
 	*a = *b;
 	*b = valeur_de_a;
@@ -11,38 +11,35 @@ void ft_swap(int *a, int *b)
 
 void ft_rev_int_tab(int *tab, int size)
 {
-	int d = 0;
-	int f = size -1;
-
-	while (d < size && f > 0)
+	int * const first_elem = tab;
+	int * const last_elem = tab + size - 1;
+	const int	mid_array = size / 2;
+	int 		count = 0;
+	
+	while (count < mid_array)
 	{
-		while (d >= f && f <= d)
-		{
-			ft_swap(&tab[d], &tab[f]);
-			++d;
-			--f;
-		}
-		++d;
-		--f;
+		ft_swap(first_elem + count, last_elem - count);
+		++count;
 	}
+}
+
+void display_array(int *ints, size_t size)
+{
+	for (size_t i = 0; i < size; ++i)
+		printf("%d ", ints[i]);
+	printf("\n");
 }
 
 int main(void)
 {
 	int tableau[] = {1, 2, 3, 4, 5, 6};
+	const size_t size = sizeof(tableau) / sizeof(tableau[0]);
 
-	ft_rev_int_tab(tableau, 6);
+	display_array(tableau, size);
 
-	int taille = sizeof(tableau) / sizeof(tableau[0]);
+	ft_rev_int_tab(tableau, (int)size);
 
-	printf("Contenu du tableau : ");
-
-	for (int i = 0; i < taille; ++i)
-	{
-		printf("%d", tableau[i]);
-	}
-
-	printf("\n");
+	display_array(tableau, size);
 
 	return 0;
 }
