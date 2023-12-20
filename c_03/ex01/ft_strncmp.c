3d42e3e478c66699bcd90d57cf21f993852e473c
Thomas Chobert <thomaschobert00@gmail.com>
2023-12-20 14:16:03 +0100

    exercice 1 du jour c03

--- c_03/ex01/ft_strncmp.c
+++ c_03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int count;
 				count = 0;
 
-	while (count < n && count < n)
+	while (count < n)
 	{
 		if (s1[count] != s2[count])
 			return s1[count] - s2[count];
@@ -18,8 +18,8 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 
 int	main(void)
 {
-	char *str1 = "";
-	char *str2 = "";
+	char *str1 = "Hello";
+	char *str2 = "World";
 
 	printf("%d\n", ft_strncmp(str1, str2, 3));
 
